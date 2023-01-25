39ef24f132e18366a7ebfb4b6ca206c42d2dfdd5
Ivaylo <ivailomn216@gmail.com>
2023-01-25 16:37:08 +0200

    Created movement

--- HowToMakeVideoGame/Assets/PlayerMovement.cs
+++ HowToMakeVideoGame/Assets/PlayerMovement.cs
@@ -5,9 +5,27 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
 
+    public float forwardForce = 2000f;
+    public float sidesMovementForce = 500f;
+
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+        //rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidesMovementForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidesMovementForce * Time.deltaTime, 0, 0);
+        }
     }
 }
